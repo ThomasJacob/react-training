87546171d4c0b7bf365a12a0f98bda59b8ea312d
Thomas <thomas.jacob@identitymine.com>
2017-07-05 12:58:06 +0530

    First Component

--- src/index.js
+++ src/index.js
@@ -2,4 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-ReactDOM.render(<div>Hello App</div>, document.getElementById('root'));
+class App extends React.Component{
+    render(){
+        return(<div>Hello React App</div>);
+    }
+}
+
+//Entry point of first Component.
+//param 1: Component
+//param 2: Dom element content should be rendered too.
+ReactDOM.render(<App/>, document.getElementById('root'));
